956764f137d3851ffe6a85535087812426252e6a
AndreasLC <andreaslykkecaspers@gmail.com>
2026-03-23 14:13:52 +0100

    Added dotnet-svcutil to WareHouse

---
+++ SkateboardAS/WareHouse/IWareHouse.cs
@@ -0,0 +1,14 @@
+﻿namespace DefaultNamespace;
+
+public interface IWareHouse
+{
+     //Properties
+     public int State { get; set;}
+     public int TrayID {get; set;}
+     
+     //Methods 
+     public void PickItem (int trayID)
+     public void InsertItem(int trayID , string name)
+     public void GetInventory();
+     
+}
