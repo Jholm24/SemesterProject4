991a8a506c9a872d231e9d9dde97ae013bf0bf88
JwSid24 <jwan.sidoo9@gmail.com>
2026-04-13 10:36:39 +0200

    Iconnect datatyper

--- SkateboardAS/Core/Interfaces/IConnect.cs
+++ SkateboardAS/Core/Interfaces/IConnect.cs
@@ -2,17 +2,17 @@ namespace SemesterProjekt4.SkateboardAS.Core.Interfaces;
 
 public interface IConnect
     {
-    public int MachineId { get; set; }
+    public Task<int> MachineId { get; set; }
     
-    public string MachineType { get; set; }
+    public Task<string> MachineType { get; set; }
 
-    public void AddMachine(int machineId, string machineType);
+    public Task AddMachine(int machineId, string machineType);
 
-    public void RemoveMachine(int machineId);
+    public Task RemoveMachine(int machineId);
 
     public Task ConnectMachine(int machineId);
 
-    public void DisconnectMachine(int machineId);
+    public Task DisconnectMachine(int machineId);
 
-    public bool IsConnected(int machineId);
+    public Task<bool> IsConnected(int machineId);
     }
